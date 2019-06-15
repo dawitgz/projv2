bcf7dd71f4b04f3fc288bb2677c3128bf3ea5a5f
Dawit <dawitgzb@yahoo.com>
2019-06-15 10:31:56 -0400

    Implemented suggested changes

--- README.md
+++ README.md
@@ -5,7 +5,50 @@ This is a catalog app that lists different gear items categorized by sport type.
 The application allows you to do CRUD operations to the catalog and get json items list using apis.
 
 #### How to build
-You can use the Vagrant file included here to build and ran using vagrant or install the required applications from requirements.txt via pip. To populate the app with test data, you will first need to ran database_setup.py to create the tables and database in sqlite. Then ran sports_items.py to populate with content. Lastly, ran application.py to start the server and there are instructions on how to access the site on your browser. Note, to get Google login to work, use localhost instead of 0.0.0.0
+You can install the required applications from requirements.txt via pip. It is best to do this using a virtual environment.
+
+0. Clone project from this github location and save the code in a folder. eg proj/folder
+1. Install virtualenv
+2. Go to project folder and create a virtual environment.
+	> cd proj/folder
+	> virtualenv venv 
+	> source venv/Scripts/activate (Windows)
+	> pip install -r requirements.txt
+	> pip list to confirm
+3. To populate the app with test data, you will first need to ran database_setup.py to create the tables and database in sqlite. 
+	> python database_setup.py
+4. Then ran sports_items.py to populate with content. 
+	> python sports_items.py
+5. Lastly, ran application.py to start the server and there are instructions on how to access the site on your browser. 
+	> python application.py
+6. Note, to get Google login to work, use 'localhost' instead of '0.0.0.0' as the instruction says.
+	> To access the application, in your browser, go to http://localhost:8000/
 
 #### Requirements
-Libraries listed under requirements.txt
+Libraries listed under requirements.txt The main ones are Flask (Application), SQLAlchemy(Database), oauth2client(Google Authentication) and requests (APIs), the rest are dependencies for those libraries. Below is a full list of requirements from pip list.
+
+Package         Version
+--------------- --------
+certifi         2019.3.9
+chardet         3.0.4
+Click           7.0
+Flask           1.0.3
+httplib2        0.12.3
+idna            2.8
+itsdangerous    1.1.0
+Jinja2          2.10.1
+MarkupSafe      1.1.1
+oauth2client    4.1.3
+pip             19.1.1
+psycopg2-binary 2.8.2
+pyasn1          0.4.5
+pyasn1-modules  0.2.5
+requests        2.22.0
+rsa             4.0
+setuptools      41.0.1
+six             1.12.0
+SQLAlchemy      1.3.4
+urllib3         1.25.3
+Werkzeug        0.15.4
+wheel           0.33.4
+
--- application.py
+++ application.py
@@ -20,6 +20,7 @@ from flask import make_response
 
 import requests
 
+import sqlalchemy
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
 
@@ -67,7 +68,7 @@ def showCatalog():
 def showLogin():
     ''' Renders the Login Template '''
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in xrange(32))
+                    for x in range(32))
     login_session['state'] = state
     return render_template('login.html', STATE=state)
 
@@ -118,7 +119,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
+        print("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -163,7 +164,7 @@ def gconnect():
                             -webkit-border-radius: 150px;\
                             -moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
-    print "done!"
+    print("done!")
     return output
 
 
@@ -172,19 +173,19 @@ def gdisconnect():
     ''' Function used to log out a Google user that is already signed in '''
     access_token = login_session.get('access_token')
     if access_token is None:
-        print 'Access Token is None'
+        print('Access Token is None')
         response = make_response(json.dumps('User not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    print 'In gdisconnect access token is %s', access_token
-    print 'User name is: '
-    print login_session['username']
+    print('In gdisconnect access token is %s', access_token)
+    print('User name is: ')
+    print(login_session['username'])
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' \
         % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print 'result is '
-    print result
+    print('result is ')
+    print(result)
     if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
@@ -220,7 +221,7 @@ def getUserInfo(user_id):
 def getUserID(email):
     ''' Given an email, returns a user ID or None '''
     try:
-        user = session.query(User).filter_by(email=email).first()
+        user = session.query(User).filter_by(email=email).one()
         return user.id
     except sqlalchemy.orm.exc.NoResultFound:
         return None
@@ -312,17 +313,17 @@ def showItem(category_name, item_name):
         .filter_by(name=item_name).first()
     creator = getUserInfo(item.user_id)
 
-    loggedIn = False
-    if ('username' in login_session and login_session['email'] != creator.email)):
-        loggedIn = True
+    owner = False
+    if ('username' in login_session) and (login_session['email'] == creator.email):
+        owner = True
 
     if (('username' not in login_session) or
             (('email' in login_session)
                 and (login_session['email'] != creator.email))):
-        return render_template('showItem.html', item=item, loggedIn=loggedIn)
+        return render_template('showItem.html', item=item, owner=owner, creator=creator)
 
     print(login_session, creator.id)
-    return render_template('showItem.html', item=item, creator=creator)
+    return render_template('showItem.html', item=item, owner=owner, creator=creator)
 
 
 @app.route(
